d0402b7331b17e0030075dfaebcb3f83b3d2b695
ferrance <dave@ferrance.com>
2023-04-11 22:44:05 -0600

    some law layer stuff

--- keymap.c
+++ keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NAV] = LAYOUT(
   QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, _______,
+  _______, DZ_CLSE, XXXXXXX, DZ_VDLT, DZ_VDRT, KC_5,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, _______,
   _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, _______,
   _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_DEL,
                              _______, _______, _______, _______, _______, KC_BSPC, _______, _______
