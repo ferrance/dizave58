2984ce51f5a9dc4b9dfabab2b72b4a9da38b2bc1
ferrance <dave@ferrance.com>
2023-06-07 15:31:34 -0600

    remove parens from num layer; remove mod tap interrupt

--- config.h
+++ config.h
@@ -32,7 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // home row mods
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
 
 #define UNICODE_SELECTED_MODES UNICODE_MODE_MACOS, UNICODE_MODE_WINCOMPOSE
 
--- keymap.c
+++ keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR,
   _______, DZ_CLSE, XXXXXXX, DZ_VDLT, DZ_VDRT, KC_PSCR,                   KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_DEL,
   _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_SCRL,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, KC_INS,
-  _______, _______, _______, _______, _______, KC_PAUS, KC_LBRC, KC_RBRC, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_CAPS,
+  _______, _______, _______, _______, _______, KC_PAUS, KC_LBRC, KC_RBRC, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_CAPS,
                              _______, _______, _______, _______, _______, KC_BSPC, _______, _______
 ),
 /*  NUMBERS
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_LBRC, KC_7,   KC_8,    KC_9,  KC_RBRC,                     XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, DZ_CLMK,
    DZ_LAW, KC_SCLN, KC_4,   KC_5,    KC_6,  KC_EQL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, XXXXXXX,
   _______, KC_GRV,  KC_1,   KC_2,    KC_3,  KC_MINS, KC_LBRC, KC_RBRC,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DZ_WIN,
-                            _______, KC_LPRN, KC_0,  KC_RPRN, _______,  _______, _______, _______
+                            _______, _______, KC_0,  _______, _______,  _______, _______, _______
 ),
 
 /* LEGAL - only useful to NM appellate lawyers who use split ortho keebs. so, just me basically
